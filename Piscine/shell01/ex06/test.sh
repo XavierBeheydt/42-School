a27a7343c3634c139335ef626147fa982a7f4654
Xavier Beheydt <xavier.beheydt@gmail.com>
2021-09-01 17:52:00 +0200

    Hardcode expected lines in ex06 test

--- Piscine/shell01/ex06/test.sh
+++ Piscine/shell01/ex06/test.sh
@@ -9,8 +9,19 @@ cp skip.sh "$SANDBOX/skip.sh"
 	cd "$SANDBOX" || exit 1
 	touch file{1,2,3,4,5,6,7}
 
+	full=$(ls -l)
+	line1=$(echo "$full" | sed -n '1p')
+	line3=$(echo "$full" | sed -n '3p')
+	line5=$(echo "$full" | sed -n '5p')
+	line7=$(echo "$full" | sed -n '7p')
+	line9=$(echo "$full" | sed -n '9p')
+	expected="$line1
+$line3
+$line5
+$line7
+$line9"
+
 	result=$(bash skip.sh)
-	expected=$(ls -l | sed -n '1~2p')
 
 	[ "$result" == "$expected" ] && echo "✅ skip.sh OK" || echo "❌ skip.sh KO"
 )
